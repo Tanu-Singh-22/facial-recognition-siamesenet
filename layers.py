28bee89c7e59088230d4407806392c69f77a0531
Tanu-Singh-22 <tragedeigh@gmail.com>
2025-06-27 11:18:25 +0530

    Add files via upload

---
+++ layers.py
@@ -0,0 +1,16 @@
+#custom l1 distance layer module 
+
+
+#import dependencies
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+class L1Dist(Layer):
+    
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
